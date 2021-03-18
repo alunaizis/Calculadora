a428c5d78554458b4af7533f9392fe34f10fdc4d
alunaizis <izisp@acad.ifms.edu.br>
2021-03-18 12:23:21 -0300

    Formulario da Calculadora

--- resources/views/soma.blade.php
+++ resources/views/soma.blade.php
@@ -5,8 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+<style>
+    body{ 
+         font-family: font-family: 'Merriweather', serif;
+}
+</style>
 </head>
 <body>
+    <div class="container">
+    <div class="row mt-2">
+    Cabeçalho
+    </div> 
+       <div class="row mt-5">
     <h1>Calculadora</h1>
+    <form>
+  <div class="mb-3">
+    <label for="Valor1" class="form-label">Primeiro Valor</label>
+    <input type="number" class="form-control" id="Valor1" aria-describedby="emailHelp">
+    
+  <div class="mb-3">
+    <label for="Valor2" class="form-label">Segundo Valor</label>
+    <input type="number" class="form-control" id="Valor2">
+  </div>
+  <button type="submit" class="btn btn-primary">Calcular</button>
+</form>
+       </div>
+    <div class="row mt-5">
+    Rodapé
+    </div>  
+    </div>
 </body>
 </html>
